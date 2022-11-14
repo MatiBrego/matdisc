e78312682ea0cc53221428c522c3309c2c0507a8
MatiBrego <matias.bregoli@ing.austral.edu.ar>
2022-11-14 11:33:35 -0300

    Fix: Added generic values to Transport Api.

--- app/src/main/java/matdisc/TransportApi.java
+++ app/src/main/java/matdisc/TransportApi.java
@@ -3,21 +3,27 @@ package matdisc;
 import java.util.TreeSet;
 
 public class TransportApi {
-    Digraph Warshall_digraph;
+
+    Digraph<Integer> Warshall_digraph;
+
+
     public TransportApi(int[][] connections){
         Digraph<Integer> digraph=new DigraphSMatrix<Integer>(10);
-        TreeSet<Integer> vertexes = new TreeSet<>();
+        TreeSet<Integer> vertexes = new TreeSet<Integer>();
         for (int i = 0; i < connections.length; i++) {
             vertexes.add(connections[i][0]);
             vertexes.add(connections[i][1]);
         }
-        DigraphApi digraphApi = new DigraphApi();
-        Object[] vertex_array =  vertexes.toArray();
+
+        DigraphApi<Integer> digraphApi = new DigraphApi<>();
+        Integer[] vertex_array = new Integer[vertexes.size()];
+        vertexes.toArray(vertex_array);
         digraphApi.putDataInGraph(vertex_array,connections,digraph);
         Warshall_digraph = digraphApi.Warshall(digraph);
-
     }
+
     public boolean isProvider(int v, int w){
+        //0 represents vertex 1 (0 based-index)
         return Warshall_digraph.existsEdge(v,w);
     }
 }
--- app/src/test/java/matdisc/TransportApiTest.java
+++ app/src/test/java/matdisc/TransportApiTest.java
@@ -1,13 +1,10 @@
 package matdisc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class TransportApiTest {
     @Test
     public void testTransport(){
-        //0 represents vertex 1 (0 based-index)
         int[][] connections = {{0,1},{0,2},{1,2},{1,4},{2,3},{2,4},{3,1},{3,4}};
         TransportApi transportApi = new TransportApi(connections);
         assertEquals(true, transportApi.isProvider(0,4));
